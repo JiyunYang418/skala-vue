0da8f08cc6db8b2bea20fdfdf6846fe38c4dd2cf
JiyunYang418 <yangjiyun0418@gmail.com>
2026-07-31 08:48:07 +0000

    .

--- WeatherMockup.vue
+++ WeatherMockup.vue
@@ -20,5 +20,11 @@ const showDetail = (cityName, status) => {
    <h3>🔎 도시 검색</h3>
    <input type="text" :value="searchQuery" @input="(e) => (searchQuery = e.target.value)" placeholder="검색할 도시 입력" />
    <p>
+   검색 중인 도시: <strong>{{ searchQuery }}</strong>
+   </p>
+
+  <section class="list-box">
+   <h3>🏞️ 지역별 날씨 현황</h3>
    
+   <div v-for="item in weatherList," :key="item.id" >
 </template>
